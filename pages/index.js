2b87a1084588879b8b2d137fe76f40a4c7c72863
kore4n <56659385+kore4n@users.noreply.github.com>
2022-09-22 00:13:20 -0400

    Revert "Update index.js"
    This reverts commit 7dad097223bbf6f786d734c5cc7a0193b89c732a.

--- pages/index.js
+++ pages/index.js
@@ -11,24 +11,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <div>
-        Hi, I'm Jesse Maltese!
-
-
-        I'm a third-year student at the University of Toronto where
-        I am pursuing a bachelor's degree in Mathematics with minors
-        in Computer Science and Philosophy with a heavy focus on logic.
-
-        In Summer 2021, I was a full stack development intern at Kontrol Energy.
-        During Summer 2022, I will be working at Hubspot in Boston, as a software engineering intern.
-
-        On this website you'll find some of my projects and my resume.
-        Occasionally I do some writing too. You can also check out
-        my simple chord + scale search.
-
-
-        Thanks for looking around! :)
-      </div>
+      <body>
+        This is a test.
+      </body>
     </div>
   )
 }
