9ffddce5cd0f1124b5f9fec58596aedc7bf38293
kore4n <56659385+kore4n@users.noreply.github.com>
2022-10-01 05:52:27 +0000

    Deploying to public from @ kore4n/kore4n.github.io@4ea4d999f53fbb08afb602b6e5668848c289cfef 🚀

---
+++ _next/static/qY7DAMch6nOKFHvb4mdH-/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-a59107631d5560ef.js","static/chunks/pages/index-2124bb8dd7559d32.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
