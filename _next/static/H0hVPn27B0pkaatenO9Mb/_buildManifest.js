a80017b9fc4dd93538bde34e21d5a07c8d691912
kore4n <56659385+kore4n@users.noreply.github.com>
2022-10-01 20:27:33 +0000

    Deploying to public from @ kore4n/kore4n.github.io@58faf83787df28556040492a8b629e17f5556c95 🚀

---
+++ _next/static/H0hVPn27B0pkaatenO9Mb/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-a59107631d5560ef.js","static/chunks/pages/index-418f4c738496eaf9.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
--- _next/static/K3K3d0a5VvxhEFj3NpOke/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/247-a59107631d5560ef.js","static/chunks/pages/index-effc95b6294101b9.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
